6249494ed6914f490acee6e9e4145be980354621
Angel <jacarvajalsoto@gmail.com>
2021-02-16 15:17:08 +0100

    all

---
+++ T5/1/main.tf
@@ -0,0 +1,36 @@
+# TODO: Designate a cloud provider, region, and credentials
+
+provider "aws" {
+  access_key = "AKIA5LPUWRZIJABIITFQ"
+  secret_key = "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"
+  region = "us-east-1"
+}
+
+
+# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+resource "aws_instance" "UdacityT2" {
+  count = 4
+  ami = "ami-0323c3dd2da7fb37d"
+  instance_type = "t2.micro"
+#  named = "Udacity_T2"
+  subnet_id = "subnet-32018d54"
+#  vpc_security_group_ids = "sg-b8408a8e"
+  tags = {
+    type = "udacity_projrct2"
+    note = "deleteme"
+  }
+}  
+
+# TODO: provision 2 m4.large EC2 instances named Udacity M4
+# resource "aws_instance" "UdacityM4" {
+#   count = 2
+#   ami = "ami-0323c3dd2da7fb37d"
+#   instance_type = "m4.large"
+# #  named = "Udacity_M4"
+#   subnet_id = "subnet-32018d54"
+# #  vpc_security_group_ids = "sg-b8408a8e"
+#   tags = {
+#     type = "udacity_projrct2"
+#     note = "deleteme"
+#   }
+# }  
---
+++ T5/2/main.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  access_key = "AKIA5LPUWRZIJABIITFQ"
+  secret_key = "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"
+  region = "${var.aws_region}"
+}
+
+
+data "archive_file" "lambda_zip" {
+    type          = "zip"
+    source_file   = "greet_lambda.py"
+    output_path   = "lambda_function.zip"
+}
+
+resource "aws_lambda_function" "udactiy_lambda" {
+  filename         = "lambda_function.zip"
+  function_name    = "udactiy_lambda"
+  role             = "${aws_iam_role.iam_for_lambda_tf.arn}"
+  handler          = "greet_lambda.lambda_handler"
+  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
+  runtime          = "python3.8"
+}
+
+resource "aws_iam_role" "iam_for_lambda_tf" {
+  name = "iam_for_lambda_tf"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+
+EOF
+
+
+
+# {
+#   "Version": "2012-10-17",
+#   "Id": "default",
+#   "Statement": [
+#     {
+#       "Sid": "InvokePermissionsForCWL0834b758c532ac26f5478b865509ef67",
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "logs.amazonaws.com"
+#       },
+#       "Action": "lambda:InvokeFunction",
+#       "Resource": "arn:aws:lambda:us-east-1:918025571920:function:udactiy_lambda",
+#       "Condition": {
+#         "StringEquals": {
+#           "AWS:SourceAccount": "918025571920"
+#         },
+#         "ArnLike": {
+#           "AWS:SourceArn": "arn:aws:logs:us-east-1:918025571920:log-group:/aws/lambda/udactiy_lambda:*"
+#         }
+#       }
+#     }
+#   ]
+# }
+}
